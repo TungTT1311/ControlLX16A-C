43d98a33eb7d7620cc2c4f219b7ac221ed114066
TungTT <17521241@gm.uit.edu.vn>
2021-04-27 01:29:21 -0700

    first commit

---
+++ testSerialLX16a.c
@@ -0,0 +1,177 @@
+/// C library headers
+#include <stdio.h>
+#include <string.h>
+
+// Linux headers
+#include <fcntl.h> // Contains file controls like O_RDWR
+#include <errno.h> // Error integer and strerror() function
+#include <termios.h> // Contains POSIX terminal control definitions
+#include <unistd.h> // write(), read(), close()
+#include <stdint.h>
+#define GET_LOW_BYTE(A) (uint8_t)((A))
+//Macro function  get lower 8 bits of A
+#define GET_HIGH_BYTE(A) (uint8_t)((A) >> 8)
+//Macro function  get higher 8 bits of A
+#define BYTE_TO_HW(A, B) ((((uint16_t)(A)) << 8) | (uint8_t)(B))
+//put A as higher 8 bits   B as lower 8 bits   which amalgamated into 16 bits integer
+
+#define LOBOT_SERVO_FRAME_HEADER         0x55
+#define LOBOT_SERVO_MOVE_TIME_WRITE      1
+#define LOBOT_SERVO_MOVE_TIME_READ       2
+#define LOBOT_SERVO_MOVE_TIME_WAIT_WRITE 7
+#define LOBOT_SERVO_MOVE_TIME_WAIT_READ  8
+#define LOBOT_SERVO_MOVE_START           11
+#define LOBOT_SERVO_MOVE_STOP            12
+#define LOBOT_SERVO_ID_WRITE             13
+#define LOBOT_SERVO_ID_READ              14
+#define LOBOT_SERVO_ANGLE_OFFSET_ADJUST  17
+#define LOBOT_SERVO_ANGLE_OFFSET_WRITE   18
+#define LOBOT_SERVO_ANGLE_OFFSET_READ    19
+#define LOBOT_SERVO_ANGLE_LIMIT_WRITE    20
+#define LOBOT_SERVO_ANGLE_LIMIT_READ     21
+#define LOBOT_SERVO_VIN_LIMIT_WRITE      22
+#define LOBOT_SERVO_VIN_LIMIT_READ       23
+#define LOBOT_SERVO_TEMP_MAX_LIMIT_WRITE 24
+#define LOBOT_SERVO_TEMP_MAX_LIMIT_READ  25
+#define LOBOT_SERVO_TEMP_READ            26
+#define LOBOT_SERVO_VIN_READ             27
+#define LOBOT_SERVO_POS_READ             28
+#define LOBOT_SERVO_OR_MOTOR_MODE_WRITE  29
+#define LOBOT_SERVO_OR_MOTOR_MODE_READ   30
+#define LOBOT_SERVO_LOAD_OR_UNLOAD_WRITE 31
+#define LOBOT_SERVO_LOAD_OR_UNLOAD_READ  32
+#define LOBOT_SERVO_LED_CTRL_WRITE       33
+#define LOBOT_SERVO_LED_CTRL_READ        34
+#define LOBOT_SERVO_LED_ERROR_WRITE      35
+#define LOBOT_SERVO_LED_ERROR_READ       36
+
+//#define LOBOT_DEBUG 1  /*Debug ：print debug value*/
+
+typedef unsigned char   byte;
+
+byte LobotCheckSum(byte buf[])
+{
+  byte i;
+  uint16_t temp = 0;
+  for (i = 2; i < buf[3] + 2; i++) {
+    temp += buf[i];
+  }
+  temp = ~temp;
+  i = (byte)temp;
+  return i;
+}
+
+void LobotSerialServoMove(uint8_t id, int16_t position, uint16_t time, int serial_port)
+{
+  byte buf[10];
+  if(position < 0)
+    position = 0;
+  if(position > 1000)
+    position = 1000;
+  buf[0] = buf[1] = LOBOT_SERVO_FRAME_HEADER;
+  buf[2] = id;
+  buf[3] = 7;
+  buf[4] = LOBOT_SERVO_MOVE_TIME_WRITE;
+  buf[5] = GET_LOW_BYTE(position);
+  buf[6] = GET_HIGH_BYTE(position);
+  buf[7] = GET_LOW_BYTE(time);
+  buf[8] = GET_HIGH_BYTE(time);
+  buf[9] = LobotCheckSum(buf);
+  write(serial_port, buf, 10);
+}
+
+int main() {
+  // Open the serial port. Change device path as needed (currently set to an standard FTDI USB-UART cable type device)
+  int serial_port = open("/dev/ttyUSB0", O_RDWR);
+  if (serial_port < 0) {
+    printf("Error %i from open: %s\n", errno, strerror(errno));
+  }
+  // Create new termios struc, we call it 'tty' for convention
+  struct termios tty;
+
+  // Read in existing settings, and handle any error
+  if(tcgetattr(serial_port, &tty) != 0) {
+      printf("Error %i from tcgetattr: %s\n", errno, strerror(errno));
+      return 1;
+  }
+
+  tty.c_cflag &= ~PARENB; // Clear parity bit, disabling parity (most common)
+  tty.c_cflag &= ~CSTOPB; // Clear stop field, only one stop bit used in communication (most common)
+  tty.c_cflag &= ~CSIZE; // Clear all bits that set the data size 
+  tty.c_cflag |= CS8; // 8 bits per byte (most common)
+  tty.c_cflag &= ~CRTSCTS; // Disable RTS/CTS hardware flow control (most common)
+  tty.c_cflag |= CREAD | CLOCAL; // Turn on READ & ignore ctrl lines (CLOCAL = 1)
+
+  tty.c_lflag &= ~ICANON;
+  tty.c_lflag &= ~ECHO; // Disable echo
+  tty.c_lflag &= ~ECHOE; // Disable erasure
+  tty.c_lflag &= ~ECHONL; // Disable new-line echo
+  tty.c_lflag &= ~ISIG; // Disable interpretation of INTR, QUIT and SUSP
+  tty.c_iflag &= ~(IXON | IXOFF | IXANY); // Turn off s/w flow ctrl
+  tty.c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL); // Disable any special handling of received bytes
+
+  tty.c_oflag &= ~OPOST; // Prevent special interpretation of output bytes (e.g. newline chars)
+  tty.c_oflag &= ~ONLCR; // Prevent conversion of newline to carriage return/line feed
+  // tty.c_oflag &= ~OXTABS; // Prevent conversion of tabs to spaces (NOT PRESENT ON LINUX)
+  // tty.c_oflag &= ~ONOEOT; // Prevent removal of C-d chars (0x004) in output (NOT PRESENT ON LINUX)
+
+  tty.c_cc[VTIME] = 10;    // Wait for up to 1s (10 deciseconds), returning as soon as any data is received.
+  tty.c_cc[VMIN] = 0;
+
+  // Set in/out baud rate to be 9600
+  cfsetispeed(&tty, B115200);
+  cfsetospeed(&tty, B115200);
+
+  // Save tty settings, also checking for error
+  if (tcsetattr(serial_port, TCSANOW, &tty) != 0) {
+      printf("Error %i from tcsetattr: %s\n", errno, strerror(errno));
+      return 1;
+  }
+
+  // Write to serial port
+  // char sWrite[] = '0x55 0x55 1 4 14 1 \r';
+  // unsigned char msg[] = { '0x55 0x55 1 4 14 1 \r' };
+  while (1)
+  {
+    LobotSerialServoMove(1, 0, 100, serial_port);
+    sleep(2);
+    LobotSerialServoMove(1, 500, 100, serial_port);
+    sleep(2);
+    LobotSerialServoMove(1, 700, 100, serial_port);
+    sleep(2);
+    LobotSerialServoMove(1, 1000, 100, serial_port);
+    sleep(2);
+  }
+  
+  LobotSerialServoMove(1, 500, 100, serial_port);
+  // char str[] = {0x55, 0x55, 1, 4, 14, 1, 0x00}; 
+  // write(serial_port,str,strlen(str)-1);
+//   write(serial_port, msg, sizeof(msg));
+  // write(serial_port, sWrite, sizeof(sWrite));
+
+  // Allocate memory for read buffer, set size according to your needs
+  char read_buf [256];
+
+  // Normally you wouldn't do this memset() call, but since we will just receive
+  // ASCII data for this example, we'll set everything to 0 so we can
+  // call printf() easily.
+  memset(&read_buf, '\0', sizeof(read_buf));
+
+  // Read bytes. The behaviour of read() (e.g. does it block?,
+  // how long does it block for?) depends on the configuration
+  // settings above, specifically VMIN and VTIME
+  int num_bytes = read(serial_port, &read_buf, sizeof(read_buf));
+
+  // n is the number of bytes read. n may be 0 if no bytes were received, and can also be -1 to signal an error.
+  if (num_bytes < 0) {
+      printf("Error reading: %s", strerror(errno));
+      return 1;
+  }
+
+  // Here we assume we received ASCII data, but you might be sending raw bytes (in that case, don't try and
+  // print it to the screen like this!)
+  printf("Read %i bytes. Received message: %s", num_bytes, read_buf);
+
+  close(serial_port);
+  return 0; // success
+}
